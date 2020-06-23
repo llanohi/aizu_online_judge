12eabca154c5ce1249f3281a17108295882806e9
rano <43287732+h5hrn@users.noreply.github.com>
2020-06-24 08:46:38 +0900

    Solve ALDS1_3_C using std::list

--- courses/lesson/ALDS1/3/ALDS1_3_C.cpp
+++ courses/lesson/ALDS1/3/ALDS1_3_C.cpp
@@ -2,101 +2,43 @@
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 using namespace std;
 
-const int LEN = 1000002;
-
-struct Node {
-  int key;
-  Node *prev, *next;
-};
-
-Node *nil;
-
-void init() {
-  nil = new Node();
-  nil->next = nil;
-  nil->prev = nil;
-}
-
-void insert(int key) {
-  Node *x = new Node();
-  x->key = key;
-  x->next = nil->next;
-  nil->next->prev = x;
-  nil->next = x;
-  x->prev = nil;
-}
-
-Node *listSearch(int key) {
-  Node *cur = nil->next;
-  while (cur != nil) {
-    if (cur->key == key) {
-      break;
-    }
-    cur = cur->next;
-  }
-  return cur;
-}
-
-void deleteNode(Node *x) {
-  if (x == nil) {
-    return;
-  }
-  x->prev->next = x->next;
-  x->next->prev = x->prev;
-}
-
-void deleteKey(int key) {
-  deleteNode(listSearch(key));
-}
-
-void deleteFirst() {
-  deleteNode(nil->next);
-}
-
-void deleteLast() {
-  deleteNode(nil->prev);
-}
-
-void printList() {
-  Node *cur = nil->next;
-  int i = 0;
-  while (cur != nil) {
-    if (i > 0) {
-      printf(" ");
-    }
-    printf("%d", cur->key);
-    cur = cur->next;
-    i++;
-  }
-  printf("\n");
-}
-
 int main() {
   int n;
   cin >> n;
 
-  init();
-
   char cmd[20];
   int key;
+  list<int> L;
   rep(i, n) {
     scanf("%s%d", cmd, &key);
     if (cmd[0] == 'i') {
-      insert(key);
+      L.push_front(key);
     } else if (cmd[0] == 'd') {
       if (strlen(cmd) > 6) {
         if (cmd[6] == 'F') {
-          deleteFirst();
+          L.pop_front();
         } else {
-          deleteLast();
+          L.pop_back();
         }
       } else {
-        deleteKey(key);
+        for (list<int>::iterator it = L.begin(); it != L.end(); it++) {
+          if (*it == key) {
+            L.erase(it);
+            break;
+          }
+        }
       }
     }
   }
 
-  printList();
+  int i = 0;
+  for (list<int>::iterator it = L.begin(); it != L.end(); it++) {
+    if (i++ > 0) {
+      printf(" ");
+    }
+    printf("%d", *it);
+  }
+  printf("\n");
 
   return 0;
 }
